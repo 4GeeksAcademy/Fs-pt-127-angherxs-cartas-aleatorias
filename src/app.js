36e17ccb329adc4fded2338217fe55211321b11b
angherxs <angherxs1996@gmail.com>
2025-11-26 16:42:02 +0000

    Cartas angherxs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+const numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+const suits = ["diamonds","hearts","spades","clubs"];
+// lo copie del generador de excusas 
+function generarCarta() {
+    const n = numbers[Math.floor(Math.random() * numbers.length)];
+    const s = suits[Math.floor(Math.random() * suits.length)];
 
+    const card = document.getElementById("card");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    card.className = "card " + s;
+    card.querySelector(".number").textContent = n;
+}
+// e probado varios y no me funciona 
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("newCard").addEventListener("click", generarCarta);
+  generarCarta();
+});
